7e89805021dcfa7b822c4564075d4b69d9503592
hlross <hlross@umich.edu>
2023-12-21 21:23:44 -0500

    clan manager front end basically works. very incomplete though

---
+++ clanmanager/model.py
@@ -0,0 +1,18 @@
+import flask
+import clanmanager
+
+import requests
+
+def get_clan_data():
+    url = "http://localhost:9001"  # Replace with your URL
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print(f"Failed to fetch data. Status code: {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Request Exception: {e}")
+        return None
--- clanmanager/views/index.py
+++ clanmanager/views/index.py
@@ -6,10 +6,72 @@ URLs include:
 """
 import flask
 import clanmanager
+from datetime import datetime
+
+def unix_time_to_readable(unix_time):
+    current_time = datetime.now()
+    timestamp = datetime.fromtimestamp(unix_time)
+    time_difference = current_time - timestamp
+
+    seconds = time_difference.total_seconds()
+
+    if seconds < 60:
+        return "a few seconds ago"
+    elif seconds < 3600:
+        minutes = int(seconds // 60)
+        return f"{minutes} minute{'s' if minutes > 1 else ''} ago"
+    elif seconds < 86400:
+        hours = int(seconds // 3600)
+        return f"{hours} hour{'s' if hours > 1 else ''} ago"
+    elif seconds < 2592000:  # Approximately 30 days
+        days = int(seconds // 86400)
+        return f"{days} day{'s' if days > 1 else ''} ago"
+    else:
+        return timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
 
 @clanmanager.app.route('/')
 def index():
     """Display / route."""
-    context = {}
+    clan_data = clanmanager.model.get_clan_data()
+    if not clan_data:
+        flask.abort(404)
+    
+    context = {
+        'clan': {
+            'name': clan_data['name'],
+            'tag': clan_data['tag'],
+            'description': clan_data['description'],
+            'badge': clan_data['badge'],
+            'level': clan_data['level'],
+            'location': clan_data['location'],
+            'type': clan_data['type'],
+            'points': clan_data['points'],
+            'builder_base_points': clan_data['builder_base_points'],
+            'member_count': clan_data['member_count'],
+            'share_link': clan_data['share_link']
+        },
+        'members': []
+    }
+
+    mems = []
+    for tag, member in clan_data['current_members'].items():
+        m = {
+            'sort_by': member['last_active'],
+            'name': member['name'],
+            'tag': tag,
+            'joined': unix_time_to_readable(member['joined']),
+            'last_active': unix_time_to_readable(member['last_active']),
+            'role': member['role'],
+            'league': member['league'],
+            'share_link': member['share_link']
+        }
+        mems.append(m)
+    
+    sorted_mems = sorted(mems, key=lambda x: x['sort_by'], reverse=True)
+
+    context['members'] = sorted_mems
+
+
+    
     return flask.render_template("index.html", **context)
