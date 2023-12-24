754db2a379e7a5da2336af9441e536081c499e52
hlross <hlross@umich.edu>
2023-12-24 10:23:20 -0500

    add proper loading member page and refactor some things to work with new utils.py

--- clanmanager/views/index.py
+++ clanmanager/views/index.py
@@ -6,36 +6,8 @@ URLs include:
 """
 import flask
 import clanmanager
-from datetime import datetime
 
-import urllib.parse
-
-def convert_to_encoded_url(input_string):
-    encoded_string = urllib.parse.quote(input_string, safe='')
-    return encoded_string
-
-def unix_time_to_readable(unix_time):
-    current_time = datetime.now()
-    timestamp = datetime.fromtimestamp(unix_time)
-    time_difference = current_time - timestamp
-
-    seconds = time_difference.total_seconds()
-
-    if seconds < 60:
-        return "recently"
-    elif seconds < 360:
-        return "a few minutes ago"
-    elif seconds < 3600:
-        minutes = int(seconds // 60)
-        return f"{minutes} minute{'s' if minutes > 1 else ''} ago"
-    elif seconds < 86400:
-        hours = int(seconds // 3600)
-        return f"{hours} hour{'s' if hours > 1 else ''} ago"
-    elif seconds < 2592000:  # Approximately 30 days
-        days = int(seconds // 86400)
-        return f"{days} day{'s' if days > 1 else ''} ago"
-    else:
-        return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+from clanmanager.utils import unix_time_to_readable, convert_to_encoded_url
 
 
 @clanmanager.app.route('/')
--- clanmanager/views/member.py
+++ clanmanager/views/member.py
@@ -2,11 +2,34 @@
 Clan manger member view
 
 URLs include:
-/<member tag excluding #>/
+/<member tag>
 """
 import flask
 import clanmanager
 
+from clanmanager.utils import unix_time_to_readable
+
+
 @clanmanager.app.route('/<member_tag>')
 def get_member(member_tag):
-    return f"{member_tag}"
+    """Display /<member tag>"""
+    clan_data = clanmanager.model.get_clan_data()
+    members = clan_data['current_members'].keys()
+
+    if member_tag not in members:
+        flask.abort(404, f"Invalid member tag '{member_tag}' (either invalid format or tag is not a current member of the clan)")
+    
+    member_data = clanmanager.model.get_member(clan_data, member_tag)
+    if member_data is None:
+        flask.abort(500, f"Server error: data of {member_tag} is empty")
+
+    context = {}
+
+    context['name'] = member_data['name']
+    context['tag'] = member_tag
+    context['role'] = member_data['role']
+    context['league'] = member_data['league']
+    context['last_active'] = unix_time_to_readable(member_data['last_active'])
+    context['joined'] = unix_time_to_readable(member_data['joined'])
+    
+    return flask.render_template("member.html", **context)
